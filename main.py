d0a32a387dd5e8bb48aa8819b93c51ed0633ef19
nabijonovdavronbek619@gmail.com <nabijonovdavronbek619@gmail.com>
2026-05-21 14:31:17 +0500

    sloqapi added

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from routes import router
 
+limiter = Limiter(default_limits=["10/minute"], key_func=get_remote_address)
+
 app = FastAPI(title="VaultX API", version="3.0.0")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(router)
 
 if __name__ == "__main__":
